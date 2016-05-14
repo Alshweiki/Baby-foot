b3020373d0e8c1446d7dbaee3bedf51f0482db76
Alshweiki <ali.shwaiky@gmail.com>
2016-05-15 01:50:38 +0200

    add photo of ball and sound effect

--- tabel.js
+++ tabel.js
@@ -2,7 +2,7 @@ var sketch = function( p ) {
 
   var img;  // Declare variable 'img'.
   //var numberOfPlyer =0 ;
-
+var song;
   p.x;
   p.y;
   p.selected =false;
@@ -12,9 +12,10 @@ var sketch = function( p ) {
 
 p.setup = function() {
   p.TableImage = p.loadImage("tabel.png");  
-  p.BubbleImage = p.loadImage("bubble.png");
+  p.BubbleImage = p.loadImage("ball.png");
   var cnv = p.createCanvas(p.tablex, p.tabley);
   cnv.position(70, 100); 
+  song = p.loadSound('Blop.mp3');
   
 }
 
@@ -23,12 +24,12 @@ p.draw = function() {
   p.image(p.TableImage, 0, 0);
   if (p.mouseIsPressed){
   	p.check(p.mouseX,p.mouseY);
-  	//alert(p.mouseX+" ........... " + p.mouseY);
-  }
+   }
  
   if(p.selected){
-    p.image(p.BubbleImage, p.x, p.y, p.BubbleImage.width/3 , p.BubbleImage.width/3 );
+    p.image(p.BubbleImage, p.x, p.y, p.BubbleImage.width/6 , p.BubbleImage.width/6 );
   }
+
 }
 
 p.check = function(mouseX,mouseY)
@@ -43,6 +44,11 @@ p.check = function(mouseX,mouseY)
     p.selected = true;
     numberOfPlyer = 1;
     changePlayer();
+    if (! song.isPlaying() ) { 
+      song.play();
+    }
+  
+    
 	}
   // first bar
 	if(mouseX > 135 && mouseX < 150 && mouseY > 80 && mouseY < 105)
@@ -52,6 +58,9 @@ p.check = function(mouseX,mouseY)
     p.selected = true;
     numberOfPlyer = 2;
     changePlayer();
+     if (! song.isPlaying() ) { 
+      song.play();
+    }
 	}
   if(mouseX > 135 && mouseX < 150 && mouseY > 238 && mouseY < 255)
   {
@@ -60,6 +69,9 @@ p.check = function(mouseX,mouseY)
     p.selected = true;
     numberOfPlyer = 3;
     changePlayer();
+     if (! song.isPlaying() ) { 
+      song.play();
+    }
   }
   // second bar
    if(mouseX > 280 && mouseX < 305 && mouseY > 35 && mouseY < 50)
@@ -69,6 +81,9 @@ p.check = function(mouseX,mouseY)
     p.selected = true;
     numberOfPlyer = 4;
     changePlayer();
+     if (! song.isPlaying() ) { 
+      song.play();
+    }
   }
 
     if(mouseX > 280 && mouseX < 305 && mouseY > 95 && mouseY < 110)
@@ -78,6 +93,9 @@ p.check = function(mouseX,mouseY)
     p.selected = true;
     numberOfPlyer = 5;
     changePlayer();
+     if (! song.isPlaying() ) { 
+      song.play();
+    }
   }
   if(mouseX > 280 && mouseX < 305 && mouseY > 155 && mouseY < 170)
   {
@@ -86,6 +104,9 @@ p.check = function(mouseX,mouseY)
     p.selected = true;
     numberOfPlyer = 6;
     changePlayer();
+     if (! song.isPlaying() ) { 
+      song.play();
+    }
   }
   if(mouseX > 280 && mouseX < 305 && mouseY > 215 && mouseY < 230)
   {
@@ -94,6 +115,9 @@ p.check = function(mouseX,mouseY)
     p.selected = true;
     numberOfPlyer = 7;
     changePlayer();
+     if (! song.isPlaying() ) { 
+      song.play();
+    }
   }
   if(mouseX > 280 && mouseX < 305 && mouseY > 275 && mouseY < 290)
   {
@@ -102,6 +126,9 @@ p.check = function(mouseX,mouseY)
     p.selected = true;
     numberOfPlyer = 8;
     changePlayer();
+     if (! song.isPlaying() ) { 
+      song.play();
+    }
   }
   // third bar 
 if(mouseX > 425 && mouseX < 445 && mouseY > 45 && mouseY < 65)
@@ -111,6 +138,9 @@ if(mouseX > 425 && mouseX < 445 && mouseY > 45 && mouseY < 65)
     p.selected = true;
     numberOfPlyer = 9;
     changePlayer();
+     if (! song.isPlaying() ) { 
+      song.play();
+    }
   }
   if(mouseX > 425 && mouseX < 445 && mouseY > 155 && mouseY < 175)
   {
@@ -119,6 +149,9 @@ if(mouseX > 425 && mouseX < 445 && mouseY > 45 && mouseY < 65)
     p.selected = true;
     numberOfPlyer = 10;
     changePlayer();
+     if (! song.isPlaying() ) { 
+      song.play();
+    }
   }
   if(mouseX > 425 && mouseX < 445 && mouseY > 260 && mouseY < 280)
   {
@@ -127,6 +160,9 @@ if(mouseX > 425 && mouseX < 445 && mouseY > 45 && mouseY < 65)
     p.selected = true;
     numberOfPlyer = 11;
     changePlayer();
+     if (! song.isPlaying() ) { 
+      song.play();
+    }
   }
 
 
@@ -143,6 +179,9 @@ if(mouseX > 570 && mouseX < 585 && mouseY > 155 && mouseY < 180)
     p.selected = true;
     numberOfPlyer = 12;
     changePlayer();
+     if (! song.isPlaying() ) { 
+      song.play();
+    }
   }
    //first bar
  if(mouseX > 495 && mouseX < 515 && mouseY > 75 && mouseY < 95)
@@ -152,6 +191,9 @@ if(mouseX > 570 && mouseX < 585 && mouseY > 155 && mouseY < 180)
     p.selected = true;
     numberOfPlyer = 14;
     changePlayer();
+     if (! song.isPlaying() ) { 
+      song.play();
+    }
   }
   if(mouseX > 495 && mouseX < 515 && mouseY > 235 && mouseY < 255)
   {
@@ -160,6 +202,9 @@ if(mouseX > 570 && mouseX < 585 && mouseY > 155 && mouseY < 180)
     p.selected = true;
     numberOfPlyer = 13;
     changePlayer();
+     if (! song.isPlaying() ) { 
+      song.play();
+    }
   }
   // second bar
   if(mouseX > 350 && mouseX < 370 && mouseY > 35 && mouseY < 55)
@@ -169,6 +214,9 @@ if(mouseX > 570 && mouseX < 585 && mouseY > 155 && mouseY < 180)
     p.selected = true;
     numberOfPlyer = 19;
     changePlayer();
+     if (! song.isPlaying() ) { 
+      song.play();
+    }
   }
   if(mouseX > 350 && mouseX < 370 && mouseY > 95 && mouseY < 110)
   {
@@ -177,6 +225,9 @@ if(mouseX > 570 && mouseX < 585 && mouseY > 155 && mouseY < 180)
     p.selected = true;
     numberOfPlyer = 18;
     changePlayer();
+     if (! song.isPlaying() ) { 
+      song.play();
+    }
   }
   if(mouseX > 350 && mouseX < 370 && mouseY > 155 && mouseY < 170)
   {
@@ -185,6 +236,9 @@ if(mouseX > 570 && mouseX < 585 && mouseY > 155 && mouseY < 180)
     p.selected = true;
     numberOfPlyer = 17;
     changePlayer();
+     if (! song.isPlaying() ) { 
+      song.play();
+    }
   }
   if(mouseX > 350 && mouseX < 370 && mouseY > 215 && mouseY < 230)
   {
@@ -193,6 +247,9 @@ if(mouseX > 570 && mouseX < 585 && mouseY > 155 && mouseY < 180)
     p.selected = true;
     numberOfPlyer = 16;
     changePlayer();
+     if (! song.isPlaying() ) { 
+      song.play();
+    }
   }
   if(mouseX > 350 && mouseX < 370 && mouseY > 275 && mouseY < 290)
   {
@@ -201,6 +258,9 @@ if(mouseX > 570 && mouseX < 585 && mouseY > 155 && mouseY < 180)
     p.selected = true;
     numberOfPlyer = 15;
     changePlayer();
+     if (! song.isPlaying() ) { 
+      song.play();
+    }
   }
  
 
@@ -213,6 +273,9 @@ if(mouseX > 570 && mouseX < 585 && mouseY > 155 && mouseY < 180)
     p.selected = true;
     numberOfPlyer = 20;
     changePlayer();
+     if (! song.isPlaying() ) { 
+      song.play();
+    }
   }
    if(mouseX > 205 && mouseX < 225 && mouseY > 160 && mouseY < 175)
   {
@@ -221,6 +284,9 @@ if(mouseX > 570 && mouseX < 585 && mouseY > 155 && mouseY < 180)
     p.selected = true;
     numberOfPlyer = 21;
     changePlayer();
+     if (! song.isPlaying() ) { 
+      song.play();
+    }
   }
    if(mouseX > 205 && mouseX < 225 && mouseY > 55 && mouseY < 70)
   {
@@ -229,6 +295,9 @@ if(mouseX > 570 && mouseX < 585 && mouseY > 155 && mouseY < 180)
     p.selected = true;
     numberOfPlyer = 22;
     changePlayer();
+     if (! song.isPlaying() ) { 
+      song.play();
+    }
   }
   
 
